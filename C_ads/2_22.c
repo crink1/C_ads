9b5341786ffb58425ab64edac8ff61ac238acadd
crin <1575460882@qq.com>
2023-02-22 22:05:26 +0800

    数组指针

---
+++ C_ads/2_22.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+//
+//void print(int(*p)[5])
+//{
+//	for (int i = 0; i < 5; i++)
+//	{
+//		printf("%d ", *((*p)+i));
+//	}
+//}
+//
+//
+//int main()
+//{
+//
+//	int arr[5] = {1,2,3,4,5};
+//	print(&arr);
+//	return 0;
+//}
+
+void print(int(*p)[5])
+{
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			printf("%d ",*((*(p+i))+j));
+		}
+	}
+}
+
+
+int main()
+{
+
+	int arr[2][5] = { 1,2,3,4,5,
+					6,7,8,9,0 };
+	print(&arr);
+	return 0;
+}
