4515e394d68af3fb686099a4584b07cf74143815
crin <1575460882@qq.com>
2023-02-27 10:26:47 +0800

    使用qsort

--- C_ads/2_25.c
+++ C_ads/2_25.c
@@ -63,50 +63,50 @@
 //	return 0;
 //}
 
-int cmp(const void* p1, const void* p2)
-{
-	return *(int*)p1 - *(int*)p2;
-}
-
-
-
-void swp(char* p1, char* p2, size_t w)
-{
-	for (int i = 0; i < w; i++)
-	{
-		char tmp = *p1;
-		*p1 = *p2;
-		*p2 = tmp;
-		p1++;
-		p2++;
-
-	}
-}
-
-void bsort(void* bease, size_t sz, size_t w, int(*p)(const void*, const void*))
-{
-	for (size_t i = 0; i < sz - 1; i++)
-	{
-		for (size_t j = 0; j < sz - i - 1; j++)
-		{
-			if (cmp((char*)bease+j*w, (char*)bease + (j+1) * w)>0)
-			{
-				
-					swp((char*)bease + j * w, (char*)bease + (j + 1) * w,w);
-			}
-		}
-	}
-}
-
-int main()
-{
-
-	int arr[] = { 9,8,7,4,5,6,3,2,1,0 };
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	bsort(arr,sz,sizeof(arr[0]),cmp);
-	for (int i = 0; i < sz; i++)
-	{
-		printf("%d ", arr[i]);
-	}
-	return 0;
-}
+//int cmp(const void* p1, const void* p2)
+//{
+//	return *(int*)p1 - *(int*)p2;
+//}
+//
+//
+//
+//void swp(char* p1, char* p2, size_t w)
+//{
+//	for (int i = 0; i < w; i++)
+//	{
+//		char tmp = *p1;
+//		*p1 = *p2;
+//		*p2 = tmp;
+//		p1++;
+//		p2++;
+//
+//	}
+//}
+//
+//void bsort(void* bease, size_t sz, size_t w, int(*p)(const void*, const void*))
+//{
+//	for (size_t i = 0; i < sz - 1; i++)
+//	{
+//		for (size_t j = 0; j < sz - i - 1; j++)
+//		{
+//			if (cmp((char*)bease+j*w, (char*)bease + (j+1) * w)>0)
+//			{
+//				
+//					swp((char*)bease + j * w, (char*)bease + (j + 1) * w,w);
+//			}
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//
+//	int arr[] = { 9,8,7,4,5,6,3,2,1,0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	bsort(arr,sz,sizeof(arr[0]),cmp);
+//	for (int i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
---
+++ C_ads/2_26.c
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+//struct std
+//{
+//	char name[20];
+//	int age;
+//};
+//
+//int cmp(const void* p1, const void* p2)
+//{
+//	return ((struct std*)p1)->age - ((struct std*)p2)->age;
+//}
+//
+//void text()
+//{
+//	struct std std1[] = { {"wdnmd",20},{"dwadaw",12} };
+//	int sz = sizeof(std1) / sizeof(std1[0]);
+//	qsort(std1, sz, sizeof(std1[0]), cmp);
+//	printf("%d", std1[0].age);
+//}
+//int main()
+//{
+//	
+//	text();
+//	return 0;
+//}
+
+void find(int arr[3][3], int row, int col, int n)
+{
+	int x = 0;
+	int y = col - 1;
+	while (x<row && y>=0)
+	{
+		if (arr[x][y] == n)
+		{
+			printf("ÏÂ±êÎª(%d,%d)\n", x, y);
+			break;
+		}
+		else if (arr[x][y] > n)
+		{
+			y--;
+		}
+		else
+		{
+			x++;
+		}
+	}
+}
+
+int main()
+{
+	int arr[3][3] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	find(arr, 3, 3, 7);
+	
+	return 0;
+}
