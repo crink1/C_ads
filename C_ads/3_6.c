392b46d15ef054d59ae160983862bbfeb80383d2
crin <1575460882@qq.com>
2023-03-06 21:44:45 +0800

    字符指针

---
+++ C_ads/3_6.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	char a[] = { "abcdef" };
+	printf("%d\n", sizeof(a));//16
+	printf("%d\n", sizeof(a + 0));//4/8 个字节
+	printf("%d\n", sizeof(*a));//4
+	printf("%d\n", sizeof(a + 1));//4/8
+	printf("%d\n", sizeof(a[1]));//4
+	printf("%d\n", sizeof(&a));//4/8
+	printf("%d\n", sizeof(*&a));
+	printf("%d\n", sizeof(&a + 1));//4/8
+	printf("%d\n", sizeof(&a[0]));//取出首元素的地址 4/8
+	printf("%d\n", sizeof(&a[0] + 1));//第二个元素的地址
+
+	return 0;
+}
