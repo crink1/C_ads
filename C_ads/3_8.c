32bcc00a04c3eb13cbf0f09cd143ce62624b6cff
crin <1575460882@qq.com>
2023-03-08 22:33:48 +0800

    指针练习

---
+++ C_ads/3_8.c
@@ -0,0 +1,22 @@
+//#include <stdio.h>
+//int main()
+//{
+//	int a[3][2] = { (0, 1), (2, 3), (4, 5) };
+//	int* p;
+//	p = a[0];
+//	printf("%d", p[0]);//?
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//int main()
+//{
+//	int a[5][5];
+//	int (*p)[4];
+//	p = a;//?
+//
+//	printf("%p,%d\n", &p[4][2] - &a[4][2], &p[4][2] - &a[4][2]);
+//
+//	return 0;
+//}
+//
