827f3296187b853a2615e426cc3ff446d6cf9b74
crin <1575460882@qq.com>
2023-03-10 23:53:51 +0800

    练习

--- C_ads/3_9.c
+++ C_ads/3_9.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
-int main()
-{
-	int money = 0;
-	scanf("%d", &money);//20
-	int total = 0;
-	if(money>0)
-		total = 2 * money - 1;
-
-	/*int empty = money;
-	int total = money;
-	while (empty >= 2)
-	{
-		total += empty / 2;
-		empty = empty / 2 + empty%2;
-	}*/
-	printf("%d\n", total);
-
-	return 0;
-}
+//#include <stdio.h>
+//int main()
+//{
+//	int money = 0;
+//	scanf("%d", &money);//20
+//	int total = 0;
+//	if(money>0)
+//		total = 2 * money - 1;
+//
+//	/*int empty = money;
+//	int total = money;
+//	while (empty >= 2)
+//	{
+//		total += empty / 2;
+//		empty = empty / 2 + empty%2;
+//	}*/
+//	printf("%d\n", total);
+//
+//	return 0;
+//}
