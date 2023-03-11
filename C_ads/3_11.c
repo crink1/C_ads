f2677a5c330a0d91820c1b812969c35a84b764a5
crin <1575460882@qq.com>
2023-03-11 23:25:33 +0800

    。

---
+++ C_ads/3_11.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <string.h>
+#include <stdio.h>
+
+//int main()
+//{
+//	//char arr[] = "abc\0def";
+//	char arr[3] = { 'a', 'b', 'c' };
+//	int len = strlen(arr);
+//	printf("%d\n", len);
+//	return 0;
+//}
+
+//strlen函数的返回类型是size_t - 无符号整型
+//int main()
+//{
+//	if (strlen("abc") - strlen("abcdef") > 0)
+//	{
+//		printf(">\n");
+//	}
+//	else
+//	{
+//		printf("<=\n");
+//	}
+//	return 0;
+//}
