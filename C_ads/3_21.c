fc738081cb679a5d37e10bf58aa5d877b609d82f
crinkle <1575460882@qq.com>
2023-03-21 22:26:19 +0800

    枚举

---
+++ C_ads/3_21.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+//enum Sex
+//{
+//	//枚举的可能取值,默认是从0开始，递增1的
+//	//枚举常量
+//	MALE,
+//	FEMALE,
+//	SECRET
+//};
+//
+//int main()
+//{
+//	enum Sex s = FEMALE;
+//	printf("%d\n", MALE);
+//	printf("%d\n", FEMALE);
+//	printf("%d\n", SECRET);
+//
+//	return 0;
+//}
+//
+
+//0 - EXIT
+//1 - PLAY
+//
+//#define EXIT 0
+//#define PLAY 1
+
+
+enum OPTION
+{
+	EXIT,
+	PLAY,
+	ADD,
+	DEL
+};
+
+int main()
+{
+	enum OPTION op = EXIT;
+	int arr[PLAY];
+
+	return 0;
