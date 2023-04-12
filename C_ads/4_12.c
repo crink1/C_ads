cba8172364e55236f2e3320161fe8234ee437af8
crink1 <1575460882@qq.com>
2023-04-12 21:38:35 +0800

    练习

---
+++ C_ads/4_12.c
@@ -0,0 +1,32 @@
+//// 计算斐波那契递归Fib的时间复杂度？
+//long long Fib(size_t N)
+//{
+//    if (N < 3)
+//        return 1;
+//
+//    return Fib(N - 1) + Fib(N - 2);
+//}
+//
+//void Func1()
+//{
+//    int a = 0;
+//    printf("%p\n", &a);
+//}
+//
+//void Func2()
+//{
+//    int b = 0;
+//    printf("%p\n", &b);
+//}
+//
+//int main()
+//{
+//    //printf("%d\n", Fib(10));
+//    int a = 0;
+//    printf("%p\n", &a);
+//
+//    Func1();
+//    Func2();
+//
+//    return 0;
+//}
